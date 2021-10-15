db1d7f12541b11c8b8b7e239d78b07643efac47d
Jakob Braun <jakob.braun@posteo.de>
2021-10-15 09:22:35 -0300

    fixed test

--- github_issue_adapter/test/test_github_issue_fetcher.py
+++ github_issue_adapter/test/test_github_issue_fetcher.py
@@ -1,5 +1,5 @@
 import unittest
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from adapter.github_issue_fetcher import GithubIssuesFetcher
 import testing_config
@@ -24,10 +24,10 @@ class GitHubIssueFetcherTest(unittest.TestCase):
 
     def test_list_issues(self):
         issue_fetcher = GithubIssuesFetcher(TEST_REPO_ORG, self.test_config.github_token())
-        before_creating_issue = datetime.utcnow()
         test_repo = self.github_client.get_repo(TEST_REPO_ORG + "/" + TEST_REPO_NAME)
         test_issue_name = "Issue For Testing github-issue-adapter"
         test_issue = test_repo.create_issue(test_issue_name)
+        before_creating_issue = test_issue.created_at - timedelta(seconds=1)
         try:
             issues = issue_fetcher.get_issues_of_repo(TEST_REPO_NAME, before_creating_issue)
             issues_of_test_repo = list(filter(lambda issue: issue.repo == TEST_REPO_NAME, issues))
