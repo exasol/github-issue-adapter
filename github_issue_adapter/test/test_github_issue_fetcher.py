1636d1ca2a9f752c2d45b76da57329bb07cf510f
jakobbraun <jakob.braun@posteo.de>
2021-10-18 14:01:35 +0200

    refactor/4: Added tests (#5)
    * refactor/4: Added tests

---
+++ github_issue_adapter/test/test_github_issue_fetcher.py
@@ -0,0 +1,41 @@
+from datetime import timedelta
+
+import pytest
+from github import Github
+
+import testing_config
+from adapter.github_issue_fetcher import GithubIssuesFetcher
+
+TEST_REPO_ORG = "exasol"
+TEST_REPO_NAME = "testing-release-robot"
+
+
+@pytest.fixture
+def test_config() -> testing_config.TestConfig:
+    return testing_config.read_test_config()
+
+
+@pytest.fixture
+def github_client(test_config: testing_config.TestConfig):
+    return Github(test_config.github_token())
+
+
+def test_list_repositories(test_config: testing_config.TestConfig):
+    issue_fetcher = GithubIssuesFetcher(TEST_REPO_ORG, test_config.github_token())
+    repositories = issue_fetcher.list_repositories()
+    assert TEST_REPO_NAME in repositories
+
+
+def test_list_issues(test_config: testing_config.TestConfig, github_client: Github):
+    issue_fetcher = GithubIssuesFetcher(TEST_REPO_ORG, test_config.github_token())
+    test_repo = github_client.get_repo(TEST_REPO_ORG + "/" + TEST_REPO_NAME)
+    test_issue_name = "Issue For Testing github-issue-adapter"
+    test_issue = test_repo.create_issue(test_issue_name)
+    before_creating_issue = test_issue.created_at - timedelta(seconds=1)
+    try:
+        issues = issue_fetcher.get_issues_of_repo(TEST_REPO_NAME, before_creating_issue)
+        issues_of_test_repo = list(filter(lambda issue: issue.repo == TEST_REPO_NAME, issues))
+        assert len(issues_of_test_repo) == 1
+        assert issues_of_test_repo[0].title == test_issue_name
+    finally:
+        test_issue.edit(state="CLOSED")
---
+++ github_issue_adapter/test/testing_config.py
@@ -0,0 +1,33 @@
+import os.path
+
+import yaml
+
+TEST_CONFIG_FILE_NAME = "test-config.yml"
+
+GITHUB_TOKEN_PROPERTY = "github_token"
+
+
+class TestConfig:
+    def __init__(self, github_token: str):
+        self._github_token = github_token
+
+    def github_token(self) -> str:
+        return self._github_token
+
+
+def read_test_config() -> TestConfig:
+    try:
+        with open(os.path.dirname(__file__) + "/../../" + TEST_CONFIG_FILE_NAME, "r") as stream:
+            parsed = yaml.safe_load(stream)
+            if GITHUB_TOKEN_PROPERTY not in parsed:
+                raise TestConfigException(
+                    "Missing required property '" + GITHUB_TOKEN_PROPERTY + "' in './" + TEST_CONFIG_FILE_NAME + "'. " +
+                    "Please set the property to your github-access token.")
+            return TestConfig(parsed[GITHUB_TOKEN_PROPERTY])
+    except FileNotFoundError:
+        raise TestConfigException("Could not find required test config './" + TEST_CONFIG_FILE_NAME + "'. "
+                                                                                                      "Please create that file in the project directory and add 'github_token: <YOUR TOKEN>'.")
+
+
+class TestConfigException(Exception):
+    pass
